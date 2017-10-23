15a683dbdc5700afdb57c6b619397d0c269bd659
Anton Ivanov <anton@ivanov.hk>
2017-10-23 16:45:39 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ *
+ */
+ 
+/** Equivalent to \p{Han} in other programming languages. */
+const HAN_REGEX = /[\u2E80-\u2E99\u2E9B-\u2EF3\u2F00-\u2FD5\u3005\u3007\u3021-\u3029\u3038-\u303B\u3400-\u4DB5\u4E00-\u9FD5\uF900-\uFA6D\uFA70-\uFAD9]/;
